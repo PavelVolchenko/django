8f7920465f869cf869c14f4652e364f989b359bd
Pavel Volchenko <113088179+PavelVolchenko@users.noreply.github.com>
2023-10-08 17:25:53 +0300

    Update settings.py

--- djangoGB/settings.py
+++ djangoGB/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jckhg(b186$dajw$d%%+#h0kp8i)ft40*nle@*u-yz**jp(9rp'
+SECRET_KEY = TOKEN
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
